c7f5bb5ebe7659d231279ded9290cba46a820e30
shobhu29 <46920235+shobhu29@users.noreply.github.com>
2019-01-22 17:29:59 +0530

    Add files via upload

---
+++ Startwith_s.java
@@ -0,0 +1,15 @@
+
+public class Startwith_s {
+
+	public static void main(String[] args) {
+		String input[]={"apple","sample","search","cat"};
+		int l=input.length;
+		for(int i=0;i<l;i++)
+		{
+			if(input[i].charAt(0)=='s')
+				System.out.println(input[i]);
+			
+		}
+	}
+
+}
